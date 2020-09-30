8458ae45c393952746c0134fcdb94372d2479143
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-09-29 19:48:57 -0700

    s

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ export default class App extends Component {
   }
 
   login = (user) => {
-    fetch(`https://git.heroku.com/intense-chamber-39011.git/api/v1/users/${user}`)
+    fetch(`https://intense-chamber-39011.herokuapp.com//api/v1/users/${user}`)
       .then(resp => {
         return resp.json()
       })
